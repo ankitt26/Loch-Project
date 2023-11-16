8e4614a15fad4c8381b23e3d4588e0ee627f6fd2
Ankit <ankit26kushwa@gmail.com>
2023-11-16 15:38:30 +0530

    Style first and second block

--- src/components/Landing_page.tsx
+++ src/components/Landing_page.tsx
@@ -1,20 +1,28 @@
 // import React from "react";
 import styles from "../styles/Landing_page.module.css";
-import First_group from './First_group';
-import Second_group from './Second_group';
-import Card_1 from './cards/Card_1';
-import Card_2 from './cards/Card_2';
-import Card_3 from './cards/Card_3';
+import First_group from "./First_group";
+import Second_group from "./Second_group";
+import Card_1 from "./cards/Card_1";
+import Card_2 from "./cards/Card_2";
+import Card_3 from "./cards/Card_3";
+import Cohorts from '../assets/Images/Cohorts.png'
 export default () => {
   return (
     <>
-      <div className={styles.main}>
-        <First_group/>
-        <Card_1/>
-        <Card_2/>
-        <Card_3/>
-        <Second_group/>
-      </div>
+      <main className={styles.main}>
+        <section className={styles.first_block}>
+          <First_group />
+          <div className={styles.cards}>
+            <Card_1 />
+            <Card_2 />
+            <Card_3 />
+          </div>
+        </section>
+        <section className={styles.second_block}>
+          <img src={Cohorts} className={styles.Cohorts} alt="Cohorts" />
+          <Second_group />
+        </section>
+      </main>
     </>
   );
 };
