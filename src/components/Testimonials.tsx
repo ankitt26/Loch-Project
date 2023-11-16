3d1a16742bf991f8a77ad0608a4663e0216bd855
Ankit <ankit26kushwa@gmail.com>
2023-11-16 16:52:04 +0530

    Add vector image

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,10 +1,11 @@
 import styles from '../styles/Testimonials.module.css'
+import Vector from '../assets/Images/Vector.png'
 export default ()=>{
     return(
         <div>
     <h4 className={styles.heading}> Testimonials</h4>
     <hr className={styles.hr}/>
-
+<img src={Vector} className={styles.vector} alt="vector"  ></img>
         </div>
     )
 }
