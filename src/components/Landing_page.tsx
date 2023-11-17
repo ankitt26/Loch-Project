8a6b1ec563f94ca935ddee488f2ff79ad8821e69
Ankit <ankit26kushwa@gmail.com>
2023-11-17 12:42:50 +0530

    Add slider on Cards

--- src/components/Landing_page.tsx
+++ src/components/Landing_page.tsx
@@ -5,25 +5,52 @@ import Second_group from "./Second_group";
 import Card_1 from "./cards/Card_1";
 import Card_2 from "./cards/Card_2";
 import Card_3 from "./cards/Card_3";
-import Cohorts from '../assets/Images/Cohorts.png'
-import Testimonials from './Testimonials';
+import Cohorts from "../assets/Images/Cohorts.png";
+import Testimonials from "./Testimonials";
+
+const Slider = () => {
+  return (
+    <div className={styles.cards}>
+      <div className="slider">
+        <div className="slide-track">
+          <div className="slide">
+            <Card_1 />
+          </div>
+          <div className="slide">
+            <Card_2 />
+          </div>
+          <div className="slide">
+            <Card_3 />
+          </div>
+          <div className="slide">
+            <Card_1 />
+          </div>
+          <div className="slide">
+            <Card_2 />
+          </div>
+          <div className="slide">
+            <Card_3 />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
 export default () => {
   return (
     <>
       <main className={styles.main}>
         <section className={styles.first_block}>
           <First_group />
-          <div className={styles.cards}>
-            <Card_1 />
-            <Card_2 />
-            <Card_3 />
-          </div>
+          <Slider/>
         </section>
         <section className={styles.second_block}>
           <img src={Cohorts} className={styles.Cohorts} alt="Cohorts" />
           <Second_group />
         </section>
-        <Testimonials/>
+        <Testimonials />
       </main>
     </>
   );
